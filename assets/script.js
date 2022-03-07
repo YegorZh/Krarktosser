15df816f9b7fb85b22cd3d775f1e843ff030c8ee
Yegor <ultrahentaiturbolord@gmail.com>
2022-03-07 16:13:04 +0200

    Most of the settings menu is hidden by default and can be made visible by pressing settings button

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,58 @@
-"use strict";
-const tossButton = document.getElementById('tossButton');
-const result = {
-    headsResult: document.getElementById('heads'),
-    tailsResult: document.getElementById('tails'),
-    totalResult: document.getElementById('total')
-};
-const settings = {
-    amount: document.getElementById('amount'),
-    krarkAmount: document.getElementById('thumb'),
-    side: document.getElementById('prio'),
-    evenSpread: document.getElementById('even'),
-    minPrio: document.getElementById('minPrio'),
-    maxPrio: document.getElementById('maxPrio'),
-    minSecond: document.getElementById('minSecond'),
-    maxSecond: document.getElementById('maxSecond')
-};
-tossButton === null || tossButton === void 0 ? void 0 : tossButton.addEventListener('click', () => {
-    let request = '?';
-    for (let key in settings) {
+var tossButton = document.getElementById('tossButton');
+var settingsButton = document.getElementById('settingsButton');
+var settingsMenu = document.getElementsByClassName('hide');
+var settingsDiv = document.getElementById('settingsDiv');
+function toggleSettings(settingsMenu, settingsDiv) {
+    for (var i = 0; i < settingsMenu.length; i++) {
+        var element = settingsMenu[i];
+        if (element.style.display === 'none') {
+            element.style.display = 'inline-block';
+            if (settingsDiv) {
+                settingsDiv.style.display = 'block';
+                settingsDiv.style.justifyContent = 'none';
+            }
+        }
+        else {
+            element.style.display = 'none';
+            if (settingsDiv) {
+                settingsDiv.style.display = 'flex';
+                settingsDiv.style.justifyContent = 'center';
+            }
+        }
+    }
+}
+toggleSettings(settingsMenu, settingsDiv);
+settingsButton === null || settingsButton === void 0 ? void 0 : settingsButton.addEventListener('click', function () {
+    toggleSettings(settingsMenu, settingsDiv);
+});
+tossButton === null || tossButton === void 0 ? void 0 : tossButton.addEventListener('click', function () {
+    var result = {
+        headsResult: document.getElementById('heads'),
+        tailsResult: document.getElementById('tails'),
+        totalResult: document.getElementById('total')
+    };
+    var settings = {
+        amount: document.getElementById('amount'),
+        krarkAmount: document.getElementById('thumb'),
+        side: document.getElementById('prio'),
+        evenSpread: document.getElementById('even'),
+        minPrio: document.getElementById('minPrio'),
+        maxPrio: document.getElementById('maxPrio'),
+        minSecond: document.getElementById('minSecond'),
+        maxSecond: document.getElementById('maxSecond')
+    };
+    var request = '?';
+    for (var key in settings) {
         if (settings[key].value)
             request += key + '=' + settings[key].value + '&';
     }
-    fetch('/api/coin' + request).then((response) => {
-        for (let key in result) {
+    fetch('http://krarktosser.herokuapp.com/api/coin' + request).then(function (response) {
+        for (var key in result) {
             if (result[key])
                 result[key].innerHTML = '-';
         }
         return response.json();
-    }).then(data => {
+    }).then(function (data) {
         if (result.headsResult)
             result.headsResult.innerHTML = data.heads;
         if (result.tailsResult)
--- assets/script.ts
+++ assets/script.ts
@@ -1,24 +1,52 @@
 const tossButton = document.getElementById('tossButton');
+const settingsButton = document.getElementById('settingsButton');
+const settingsMenu = document.getElementsByClassName('hide');
+const settingsDiv = document.getElementById('settingsDiv');
 
-const result: {[key: string]: any} = {
-    headsResult: document.getElementById('heads'),
-    tailsResult: document.getElementById('tails'),
-    totalResult: document.getElementById('total')
+function toggleSettings(settingsMenu: HTMLCollectionOf<Element>, settingsDiv: HTMLElement){
+    for(let i = 0; i < settingsMenu.length; i++){
+        let element = settingsMenu[i] as HTMLBaseElement;
+        if(element.style.display === 'none') {
+            element.style.display = 'inline-block';
+            if(settingsDiv) {
+                settingsDiv.style.display = 'block';
+                settingsDiv.style.justifyContent = 'none';
+            }
+        }
+        else {
+            element.style.display = 'none';
+            if(settingsDiv) {
+                settingsDiv.style.display = 'flex';
+                settingsDiv.style.justifyContent = 'center';
+            }
+        }
+    }
 }
+toggleSettings(settingsMenu, settingsDiv as HTMLElement);
 
-const settings: {[key: string]: any} = {
-    amount: document.getElementById('amount') as HTMLInputElement,
-    krarkAmount: document.getElementById('thumb') as HTMLInputElement,
-    side: document.getElementById('prio') as HTMLInputElement,
-    evenSpread: document.getElementById('even') as HTMLInputElement,
-    minPrio: document.getElementById('minPrio') as HTMLInputElement,
-    maxPrio: document.getElementById('maxPrio') as HTMLInputElement,
-    minSecond: document.getElementById('minSecond') as HTMLInputElement,
-    maxSecond: document.getElementById('maxSecond') as HTMLInputElement
-}
 
+settingsButton?.addEventListener('click', () => {
+    toggleSettings(settingsMenu, settingsDiv as HTMLElement);
+});
 
 tossButton?.addEventListener('click', () => {
+    const result: {[key: string]: any} = {
+        headsResult: document.getElementById('heads'),
+        tailsResult: document.getElementById('tails'),
+        totalResult: document.getElementById('total')
+    }
+  
+    const settings: {[key: string]: any} = {
+        amount: document.getElementById('amount') as HTMLInputElement,
+        krarkAmount: document.getElementById('thumb') as HTMLInputElement,
+        side: document.getElementById('prio') as HTMLInputElement,
+        evenSpread: document.getElementById('even') as HTMLInputElement,
+        minPrio: document.getElementById('minPrio') as HTMLInputElement,
+        maxPrio: document.getElementById('maxPrio') as HTMLInputElement,
+        minSecond: document.getElementById('minSecond') as HTMLInputElement,
+        maxSecond: document.getElementById('maxSecond') as HTMLInputElement
+    }
+
     let request = '?';
     for(let key in settings) {
         if(settings[key].value) request += key+'='+settings[key].value+'&';
